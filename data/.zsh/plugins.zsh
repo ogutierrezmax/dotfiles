1bc69fc74b81ec5974cc8a94a39022a1dc4f8e03
Max Gutierrez <219744831+ogutierrezmax@users.noreply.github.com>
2026-06-21 15:29:19 -0300

    feat(zsh): add fzf to plugins list
    Enables fzf integration via oh-my-zsh plugin for fuzzy finding
    in shell completions and history search.
    
    Verified-By: dotfiles-secure-commit

--- data/.zsh/plugins.zsh
+++ data/.zsh/plugins.zsh
@@ -5,7 +5,7 @@ export ZSH="$HOME/.oh-my-zsh"
 ZSH_THEME="powerlevel10k/powerlevel10k"
 
 # Plugins
-plugins=(tmux git zsh-autosuggestions zsh-syntax-highlighting)
+plugins=(fzf tmux git zsh-autosuggestions zsh-syntax-highlighting)
 
 # Plugin configurations
 ZSH_TMUX_AUTOSTART=false
