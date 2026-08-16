bdc62f7d01a9fa04aed0a67bb3c4bd70dd2b4d72
Max Gutierrez <219744831+ogutierrezmax@users.noreply.github.com>
2026-08-16 16:35:17 -0300

    chore(zsh): document plugins and remove recursive symlink
    Notes the active plugin list as a comment and drops the self-referential data/.zsh/.zsh link left by the machine copy.
    
    Verified-By: dotfiles-secure-commit

--- data/.zsh/plugins.zsh
+++ data/.zsh/plugins.zsh
@@ -5,8 +5,8 @@ export ZSH="$HOME/.oh-my-zsh"
 ZSH_THEME="powerlevel10k/powerlevel10k"
 
 # Plugins
+## plugins=(fzf tmux git zsh-autosuggestions zsh-syntax-highlighting)
 plugins=(fzf tmux git zsh-autosuggestions zsh-syntax-highlighting)
-
 # Plugin configurations
 ZSH_TMUX_AUTOSTART=false
 ZSH_TMUX_AUTOCONNECT=true
